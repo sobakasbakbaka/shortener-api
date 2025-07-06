07b338ac61b278afab6d7cef7aa916ff105c2c8d
Nikita <108035712+sobakasbakbaka@users.noreply.github.com>
2025-07-07 02:14:34 +0500

    Update greeting message to 'Hello World!' in service and endpoint test

--- backend/test/app.e2e-spec.ts
+++ backend/test/app.e2e-spec.ts
@@ -16,6 +16,9 @@ describe('AppController (e2e)', () => {
   });
 
   it('/ (GET)', () => {
-    return request(app.getHttpServer()).get('/').expect(200).expect('Hello!');
+    return request(app.getHttpServer())
+      .get('/')
+      .expect(200)
+      .expect('Hello World!');
   });
 });
